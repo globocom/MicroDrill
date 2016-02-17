6d4555afe516549420153b8fe738211f701f80d1
Diogo Munaro Vieira <diogo.munaro@corp.globo.com>
2016-02-17 15:23:39 -0200

    Updated license

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     license='Apache',
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7',
         ],
     packages=find_packages(
